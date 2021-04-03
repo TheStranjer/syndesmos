1caae18fb12835ac4be8200b83900c1fbf80f46a
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2021-04-03 01:27:31 -0600

    Don't even consider looking at the remote if there's no bearer token

--- lib/login.rb
+++ lib/login.rb
@@ -1,6 +1,6 @@
 class Syndesmos
   def valid_credentials?
     attempt = req(path: "/api/v1/accounts/verify_credentials")
-    attempt.class == Hash and attempt.keys.include?('acct') and not attempt.keys.include?("error")
+    bearer_token.to_s.length > 0 and attempt.class == Hash and attempt.keys.include?('acct') and not attempt.keys.include?("error")
   end
 end
