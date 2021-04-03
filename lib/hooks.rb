b343a5c1e7cc3f3fd0142ffb2b4336a83d9c5712
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2021-04-03 11:42:16 -0600

    Capability for notification 'hooks'

---
+++ lib/hooks.rb
@@ -0,0 +1,33 @@
+class Syndesmos
+  def add_hook(type, object, meth=nil)
+    hooks[type] = [] if hooks[type].nil?
+
+    hooks[type].push({ :object => object, :method => meth ? meth : type})
+  end
+
+  def clear_hooks(type)
+    hooks[type] = []
+  end
+
+  private
+
+  attr_accessor :last_notification_id
+
+  def execute_hooks
+    while true
+      handle_notifications if hooks[:notification] and hooks[:notification].length > 0
+      sleep 60
+    end
+  end
+
+  def handle_notifications
+    self.last_notification_id = notifications({ "with_muted" => true, "limit" => 20 }).collect { |notif| notif['id'].to_i }.max if self.last_notification_id.nil?
+
+    notifications({ "with_muted" => true, "limit" => 20, "since_id" => self.last_notification_id }).each do |notif|
+      self.last_notification_id = notif['id'].to_i if notif['id'].to_i > self.last_notification_id
+      hooks[:notification].each do |hook|
+        hook[:object].send(hook[:method], notif)
+      end
+    end
+  end
+end
--- lib/syndesmos.rb
+++ lib/syndesmos.rb
@@ -3,6 +3,7 @@ require 'json'
 require_relative 'generics'
 require_relative 'login'
 require_relative 'media'
+require_relative 'hooks'
 
 class Syndesmos
   attr_reader :bearer_token, :username, :password, :instance, :api
@@ -10,6 +11,7 @@ class Syndesmos
   def initialize(bearer_token: nil, username: nil, password: nil, instance:, api: Net::HTTP)
     @instance = instance
     @api = api
+    @hooks = {}
     if bearer_token
       @bearer_token = bearer_token
     elsif username and password
@@ -17,12 +19,18 @@ class Syndesmos
       login = req(path: "/oauth/token", http_method: :post, params: { 'username' => username, 'password' => password, 'client_id' => callback['client_id'], 'client_secret' => callback['client_secret'], 'grant_type' => 'password' })
       @bearer_token = login['access_token']
     end
+
+    Thread.new { execute_hooks }
   end
 
   private
 
+  attr_accessor :hooks
+
   def req(path:, http_method: :get, params: {})
-    uri = URI.parse("https://#{instance}#{path}")
+    url = "https://#{instance}#{path}" 
+    url += "?" + params.reject { |k,v| v.nil? }.collect {|k,v| "#{k}=#{v}" }.join('&' ) if http_method == :get and params.length > 0
+    uri = URI.parse(url)
 
     header = { 'Content-Type': 'application/json' }
     header['Authorization'] = "Bearer #{bearer_token}" if bearer_token
@@ -31,7 +39,7 @@ class Syndesmos
     http.use_ssl = true
 
     req = http_method_obj(http_method).new(uri.request_uri, header)
-    req.body = params.to_json
+    req.body = params.to_json if http_method != :get
 
     res = http.request(req)
 
