5302387af5cd73c1dbc1c19ab8a16567fb57603c
The Stranjer <791672+TheStranjer@users.noreply.github.com>
2021-04-04 02:46:15 -0600

    Add GET and POST relay methods

--- lib/generics.rb
+++ lib/generics.rb
@@ -5,14 +5,10 @@ class Syndesmos
     "/api/v1/timelines/home",
     "/api/v1/timelines/direct",
     "/api/v1/timelines/public",
-    "/api/v1/notifications"
+    "/api/v1/notifications",
+    "/api/pleroma/admin/relay",
+    "/api/v1/instance/peers"
   ]
-
-  GENERIC_GETS.each do |get|
-    define_method(get.split('/').last.to_sym) do |params = {}|
-      req(path: get, http_method: :get, params: params)
-    end
-  end
 
   GENERIC_POSTS = [
     "/api/v1/statuses/$ID/favourite",
@@ -22,11 +18,26 @@ class Syndesmos
     "/api/v1/statuses/$ID/follow",
     "/api/v1/statuses/$ID/unfollow",
     "/api/v1/pleroma/accounts/$ID/unsubscribe",
-    "/api/v1/statuses"
+    "/api/v1/statuses",
+    "/api/pleroma/admin/relay"
   ]
 
+  def self.generic_name(path, http_method)
+    if GENERIC_GETS.include?(path) and GENERIC_POSTS.include?(path)
+      "#{http_method}_#{path.split('/').last}".to_sym
+    else
+      path.split('/').last.to_sym
+    end
+  end
+
+  GENERIC_GETS.each do |get|
+    define_method(Syndesmos.generic_name(get, :get)) do |params = {}|
+      req(path: get, http_method: :get, params: params)
+    end
+  end
+
   GENERIC_POSTS.each do |post|
-    meth_name = post.split('/').last.to_sym
+    meth_name = Syndesmos.generic_name(post, :post)
     if post.include?("$ID")
       define_method(meth_name) do |id, params={}|
         req(path: post.gsub("$ID", id), http_method: :post, params: params)
